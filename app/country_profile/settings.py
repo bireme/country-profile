07854c66e5492d97f91298ae57bef251c4e4cb18
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2022-06-08 12:21:52 -0300

    Ajustes settings

--- app/country_profile/settings.py
+++ app/country_profile/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0vj)!xx-d#^k94yw8y@zift6(ceis8lqwtfqwgc915hkhc+s&r'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
