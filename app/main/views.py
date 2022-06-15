05e7b675b2779b16d7c4099197460f5292d00646
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2022-06-15 11:53:16 -0300

    Implementa ordenamento pelo código dos indicadores.  Close #7

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.db.models.functions import Substr
 
-from main.models import Profile
+from main.models import Profile, Indicator
 
 def index(request):
 
     profile_lists = Profile.objects.all()
+    indicator_list = Indicator.objects.all()
+
+    # sort spliting the code in parts and using the unicode value for the first part. ex. C1.5.1 -> C1 5 1
+    indicator_list_sorted = sorted(indicator_list, key=lambda x: [int(part) if part.isdigit() else ord(part[0:1]) for part in x.code.split('.')])
 
     response_data = {'profile_lists' : profile_lists}
 
